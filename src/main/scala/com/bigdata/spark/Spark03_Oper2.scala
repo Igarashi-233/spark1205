0dca0986b4fbd58c07703093b36f1f60a8c93393
Igarashi <igarashi233@sina.com>
2020-12-25 12:06:47 +0800

    学习进度 +1

---
+++ src/main/scala/com/bigdata/spark/Spark03_Oper2.scala
@@ -0,0 +1,23 @@
+package com.bigdata.spark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark03_Oper2 {
+  def main(args: Array[String]): Unit = {
+
+    val config: SparkConf = new SparkConf().setMaster("local[*]").setAppName("RDD")
+
+    val sc = new SparkContext(config)
+
+    val listRDD: RDD[Int] = sc.makeRDD(1 to 10)
+
+    // mapPartitions可以对一个RDD分区中所有分区进行遍历
+    val mapPartitionsRDD: RDD[Int] = listRDD.mapPartitions(datas => {
+      datas.map(data => data * 2)
+    })
+
+    mapPartitionsRDD.collect().foreach(println)
+
+  }
+}
